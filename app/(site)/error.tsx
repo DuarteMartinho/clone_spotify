8ed2499e882a513cad58121d669003a20204f6cf
DuarteMartinho <duartemartinho11@gmail.com>
2024-01-10 04:35:01 +0000

    13: added error and loading

---
+++ app/(site)/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Box from "@/components/Box";
+
+const Error = () => {
+    return (
+        <Box className="h-full flex items-center justify-center">
+            <p className="text-neutral-400 text-xl">Something went wrong.</p>
+        </Box>
+    );
+}
+
+export default Error;
